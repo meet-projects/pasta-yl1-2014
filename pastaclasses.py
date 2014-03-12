92dcb62d9c47fc3c08b238fe7dac9683a881106f
Aseel Bataha <aseel13@meet.mit.edu>
2014-03-12 17:57:53 +0200

    sdf

--- pastaclasses.py
+++ pastaclasses.py
@@ -3,35 +3,36 @@ class Pasta(object):
 
 		self.name=name
 		self.price=price
-		self.picture=picture
-a= Pasta("Fusilli",10,"fusilli.jpg"  )
-b=Pasta("Penne",8, "penne_rigate.jpg")
-c=Pasta("Forfalle",7,"Farfalle.jpg")
-d=Pasta("Maccheroni",9,"Maccheroni.jpg")
-e=Pasta("Cenneroni",12,"cenn.jpg")
+		self.picture = pygame.image.load(picture)
 
+	
 class Pasta_sauces(object):
 	def __init__(self,name,price,picture):
 		self.name=name
 		self.price=price
-		self.picture=picture
-f=Pasta_sauces("fresh tomato",2,"Fresh_Tomato_sauce.jpg")
-g=Pasta_sauces("white bean",3,"white_bean_sauce.jpg")
-h=Pasta_sauces("sauteed_garlic",4,"sauteed garlic.jpg")
-i=Pasta_sauces("caprese",1,"caprese sauce.jpg")
-j=Pasta_sauces("carbonara",5,"carbonara sauce.jpg")
+		self.picture=pygame.image.load(picture)
+
 
 class Extra_for_pasta(object):
 	def __init__(self,name,price,picture):
-		
 		self.name=name
 		self.price=price
-		self.picture=picture
+		self.picture=pygame.image.load(picture)
 
-k=Extra_for_pasta("sweet potato",1,"sweet-potato.jpg)
-l=Extra_for_pasta("mushroom",2,"mushroom1.jpg")
-m=Extra_for_pasta("cheese,3,""cheese1.jpg)
-n=Extra_for_pasta("olives",1,"olives1.jpg)
-o=Extra_for_pasta("tuna",4,"tuna1.jpg)
+kb=Extra_for_pasta("sweet potato",1,"sweet-potato.jpg")
+lb=Extra_for_pasta("mushroom",2,"mushroom1.jpg")
+mb=Extra_for_pasta("cheese,3,""cheese1.jpg")
+bn=Extra_for_pasta("olives",1,"olives1.jpg")
+ob=Extra_for_pasta("tuna",4,"tuna1.jpg") 
 
+ab= Pasta("Fusilli",10,"fusilli.jpg"  )
+bb=Pasta("Penne",8, "penne_rigate.jpg")
+cb=Pasta("Forfalle",7,"Farfalle.jpg")
+db=Pasta("Maccheroni",9,"Maccheroni.jpg")
+eb=Pasta("Cenneroni",12,"cenn.jpg")
 
+fb=Pasta_sauces("fresh tomato",2,"Fresh_Tomato_sauce.jpg")
+gb=Pasta_sauces("white bean",3,"white_bean_sauce.jpg")
+hb=Pasta_sauces("sauteed_garlic",4,"sauteed garlic.jpg")
+ib=Pasta_sauces("caprese",1,"caprese sauce.jpg")
+jb=Pasta_sauces("carbonara",5,"carbonara sauce.jpg")
